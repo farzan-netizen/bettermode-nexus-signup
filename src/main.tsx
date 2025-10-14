04cc487ecb7885b4969029a376cf87a804c7a0a0
farzan <farzan@bettermode.com>
2025-10-14 19:25:59 +0330

    feat: add route paths constants

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ import { ThemeProvider } from '@/providers/theme'
 import { WidgetConfigProvider } from '@/providers/widget-config-provider'
 import { ErrorBoundary } from '@/components/error-boundary'
 import { AdminProvider } from '@/hooks/use-admin'
+import { RoutePaths } from '@/constants/routes'
 import '@/styles/globals.css'
 
 createRoot(document.getElementById('root')!).render(
@@ -20,9 +21,12 @@ createRoot(document.getElementById('root')!).render(
             <BrowserRouter>
               <RouteProvider>
                 <Routes>
-                  <Route path="/" element={<Navigate to="/signup" />} />
-                  <Route path="/signup" element={<SignupPage />} />
-                  <Route path="/wizard" element={<WizardPage />} />
+                  <Route
+                    path={RoutePaths.HOME}
+                    element={<Navigate to={RoutePaths.SIGNUP} />}
+                  />
+                  <Route path={RoutePaths.SIGNUP} element={<SignupPage />} />
+                  <Route path={RoutePaths.WIZARD} element={<WizardPage />} />
                   <Route path="*" element={<NotFound />} />
                 </Routes>
               </RouteProvider>
