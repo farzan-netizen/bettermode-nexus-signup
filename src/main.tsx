bb22a30a591cdc6b56ca6420e9407fd2154d3a11
farzan <farzan@bettermode.com>
2025-10-15 00:22:34 +0330

    feat: add graphql

--- src/main.tsx
+++ src/main.tsx
@@ -10,30 +10,40 @@ import { WidgetConfigProvider } from '@/providers/widget-config-provider'
 import { ErrorBoundary } from '@/components/error-boundary'
 import { AdminProvider } from '@/hooks/use-admin'
 import { RoutePaths } from '@/constants/routes'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloProvider } from '@apollo/client/react'
+
 import '@/styles/globals.css'
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: import.meta.env.VITE_GQL_GLOBAL_ENDPOINT }),
+  cache: new InMemoryCache(),
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
-      <AdminProvider>
-        <ThemeProvider>
-          <WidgetConfigProvider>
-            <BrowserRouter>
-              <RouteProvider>
-                <Routes>
-                  <Route
-                    path={RoutePaths.HOME}
-                    element={<Navigate to={RoutePaths.SIGNUP} />}
-                  />
-                  <Route path={RoutePaths.SIGNUP} element={<SignupPage />} />
-                  <Route path={RoutePaths.WIZARD} element={<WizardPage />} />
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </RouteProvider>
-            </BrowserRouter>
-          </WidgetConfigProvider>
-        </ThemeProvider>
-      </AdminProvider>
+      <ApolloProvider client={client}>
+        <AdminProvider>
+          <ThemeProvider>
+            <WidgetConfigProvider>
+              <BrowserRouter>
+                <RouteProvider>
+                  <Routes>
+                    <Route
+                      path={RoutePaths.HOME}
+                      element={<Navigate to={RoutePaths.SIGNUP} />}
+                    />
+                    <Route path={RoutePaths.SIGNUP} element={<SignupPage />} />
+                    <Route path={RoutePaths.WIZARD} element={<WizardPage />} />
+                    <Route path="*" element={<NotFound />} />
+                  </Routes>
+                </RouteProvider>
+              </BrowserRouter>
+            </WidgetConfigProvider>
+          </ThemeProvider>
+        </AdminProvider>
+      </ApolloProvider>
     </ErrorBoundary>
   </StrictMode>,
 )
--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,1 +1,16 @@
 /// <reference types="vite/client" />
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+interface ViteTypeOptions {
+  // By adding this line, you can make the type of ImportMetaEnv strict
+  // to disallow unknown keys.
+  // strictImportMetaEnv: unknown
+}
+
+interface ImportMetaEnv {
+  readonly VITE_GQL_GLOBAL_ENDPOINT: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
