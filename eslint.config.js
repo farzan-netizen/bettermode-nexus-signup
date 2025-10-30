65b724d9c3dea2148e8bd718db09b2fa32491ab5
farzan <farzan@bettermode.com>
2025-10-30 20:17:27 +0330

    feat: set package manager

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,18 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
-import prettierConfig from 'eslint-plugin-prettier/recommended'; 
+import prettierConfig from 'eslint-plugin-prettier/recommended';
 import unusedImports from "eslint-plugin-unused-imports";
+import globals from 'globals'
 
 export default defineConfig([
   globalIgnores(['dist', "node_modules"]),
-    reactHooks.configs.flat.recommended,
+  reactHooks.configs.flat.recommended,
   {
     plugins: {
-        "unused-imports": unusedImports,
+      "unused-imports": unusedImports,
     },
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -27,17 +27,17 @@ export default defineConfig([
     },
     rules: {
       'prettier/prettier': 'error',
-      'react/react-in-jsx-scope': 'off', 
+      'react/react-in-jsx-scope': 'off',
       "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
       "unused-imports/no-unused-imports": "error",
       "unused-imports/no-unused-vars": [
-          "warn",
-          {
-              "vars": "all",
-              "varsIgnorePattern": "^_",
-              "args": "after-used",
-              "argsIgnorePattern": "^_",
-          },
+        "warn",
+        {
+          "vars": "all",
+          "varsIgnorePattern": "^_",
+          "args": "after-used",
+          "argsIgnorePattern": "^_",
+        },
       ],
     },
   },
