e6e72b4391c0b7521e5c92ebed5cf878581a39ab
farzan <farzan@bettermode.com>
2025-11-01 00:23:07 +0330

    fix: cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,7 @@ import { SignupPage } from '@/pages/signup'
 import { WizardPage } from '@/pages/wizard'
 import { RouteProvider } from '@/providers/router-provider'
 import { ThemeProvider } from '@/providers/theme'
-import { WidgetConfigProvider } from '@/providers/widget-config-provider'
 import { ErrorBoundary } from '@/components/error-boundary'
-import { AdminProvider } from '@/hooks/use-admin'
 import { RoutePaths } from '@/constants/routes'
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 import { ApolloProvider } from '@apollo/client/react'
@@ -27,31 +25,21 @@ createRoot(document.getElementById('root')!).render(
     <ErrorBoundary>
       <ReduxProvider store={store}>
         <ApolloProvider client={client}>
-          <AdminProvider>
-            <ThemeProvider>
-              <WidgetConfigProvider>
-                <BrowserRouter>
-                  <RouteProvider>
-                    <Routes>
-                      <Route
-                        path={RoutePaths.HOME}
-                        element={<Navigate to={RoutePaths.SIGNUP} />}
-                      />
-                      <Route
-                        path={RoutePaths.SIGNUP}
-                        element={<SignupPage />}
-                      />
-                      <Route
-                        path={RoutePaths.WIZARD}
-                        element={<WizardPage />}
-                      />
-                      <Route path="*" element={<NotFound />} />
-                    </Routes>
-                  </RouteProvider>
-                </BrowserRouter>
-              </WidgetConfigProvider>
-            </ThemeProvider>
-          </AdminProvider>
+          <ThemeProvider>
+            <BrowserRouter>
+              <RouteProvider>
+                <Routes>
+                  <Route
+                    path={RoutePaths.HOME}
+                    element={<Navigate to={RoutePaths.SIGNUP} />}
+                  />
+                  <Route path={RoutePaths.SIGNUP} element={<SignupPage />} />
+                  <Route path={RoutePaths.WIZARD} element={<WizardPage />} />
+                  <Route path="*" element={<NotFound />} />
+                </Routes>
+              </RouteProvider>
+            </BrowserRouter>
+          </ThemeProvider>
         </ApolloProvider>
       </ReduxProvider>
     </ErrorBoundary>
